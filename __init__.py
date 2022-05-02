e4ec8b197cd8cf6fc00720c9011e0f5afb84c2cd
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-05-02 15:31:26 +0300

    imported the Devconfig from the configuratios

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from .config import DevConfig
 
-loise = Flask(__name__)
+loise = Flask(__name__,instance_relative_config=True)
 loise .config.from_object(DevConfig)
+loise.config.from_pyfile('config.py')
 
 from app import views
