6842350e96b8e2c501e0e6a6c99a2ea6ae1e7ad0
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-05-03 07:24:01 +0300

    defined news api key and base ur;

--- request.py
+++ request.py
@@ -1,3 +1,30 @@
+from concurrent.futures import process
+import json
 from app import loise
+import urllib.request ,json
+from .models import news
 
-api_key = loise.config['NEWS_API_KEY']
+new= news.News()
+
+api_key = loise.config['NEWS_API_KEY'] #f51300cb046c40219d0804da1c5cd5eb
+
+base_url = loise.config['NEWS_API_BASE_URL'] 
+
+def get_news(category):
+    get_news_url = base_url.format(category, api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read() 
+        get_news_response= json.loads(get_news_data)
+
+        news_results=None
+
+        if get_news_response['results']
+        news_results= process_results{news_results_list}
+
+        return news_results
+
+    def process_results(news_list):
+        news_results= []
+        if item in news_list:
+            
